ea11f6ddb0c1973b240b4b648e61749f0170a448
j0sh77 <jocjohns@mtu.edu>
2016-01-05 14:09:11 -0600

    Accounted for housing error with ewads rooms showing as wwads.

--- generate.php
+++ generate.php
@@ -27,7 +27,7 @@
 		if(strpos($person, "nbsp;") || count($td) < 8){
 			file_put_contents("test.txt", "$person\n", FILE_APPEND);
 			continue;
-		} 
+		}
 		$room = strip_tags($td[5]->innertext);
 		if($min != "" && intval($room) < $min) continue;
 		if($max != "" && intval($room) > $max) continue;
@@ -38,17 +38,23 @@
 		$firstParts = explode(" ", trim($nameParts[1]));
 		$pOut = str_replace("#first#", ucwords(strtolower(trim(str_replace(",", "", $firstParts[0])))), $pOut);
 		$pOut = str_replace("#last#", str_replace(" ", "", ucwords(strtolower(trim(str_replace(",", "", $nameParts[0]))))), $pOut);
-		
+
 		$pOut = str_replace("#gender#", trim($td[1]->innertext), $pOut);
 		$pOut = str_replace("#age#", trim($td[2]->innertext), $pOut);
 		$pOut = str_replace("#class#", trim($td[3]->innertext), $pOut);
-		$pOut = str_replace("#build#", trim($td[4]->innertext), $pOut);
+
+		$building = trim($td[4]->innertext);
+		//if room contains e, make it ew or em
+		if(strpos(strip_tags($td[5]->innertext), 'E') !== false)
+			$building = "E" . substr($building, -1);
+
+		$pOut = str_replace("#build#", $building, $pOut);
 		$pOut = str_replace("#room#", trim(strip_tags($td[5]->innertext)), $pOut);
 		$pOut = str_replace("#phone#", trim($td[6]->innertext), $pOut);
 		$pOut = str_replace("#email#", trim(strip_tags($td[7]->innertext)), $pOut);
 
 		$pOut = str_replace("#id#", str_replace("@mtu.edu", "", trim(strip_tags($td[7]->innertext))), $pOut);
-	
+
 		$output .= "$pOut\n";
 	}
 
